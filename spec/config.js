740b96f583f3db9e571ecca1cb7ccb26efbcb7eb
Tom Yam <tomyam1@gmail.com>
2017-03-01 14:21:02 +0200

    several improvments for specs -
    use dotenv for loading config variables
    start and close nailgun server in the specs

---
+++ spec/config.js
@@ -0,0 +1,54 @@
+'use strict';
+const Yargs = require('yargs');
+
+
+const internals = {};
+
+/**
+ Either config image-diff-tester using command line arguments or using environment variables.
+
+ Rebase the default version
+
+ npm test -- --image-diff-rebase
+
+ Rebase another version
+
+ npm test -- --image-diff-rebase --image-diff-version ubuntu
+
+ */
+internals.getImageDiffOptions = function () {
+    const argv = Yargs.option('image-diff-rebase', {
+        type: 'boolean',
+        default: null
+    }).option('image-diff-version', {
+        type: 'string',
+        nargs: 1,
+        demand: false,
+        default: null
+    }).argv;
+
+    const imageDiffBaseVersion = argv.imageDiffVersion || process.env.IMAGE_DIFF_BASE_VERSION || 'default';
+    const imageDiffMode = (argv.imageDiffRebase ? 'rebase' : null) || process.env.IMAGE_DIFF_MODE || 'test';
+
+    return {
+        baseDir: `images-diff/base/${imageDiffBaseVersion}`,
+        mode: imageDiffMode,
+        currentDir: 'images-diff/current',
+        diffDir: 'images-diff/diff',
+        mismatchThreshold: 0
+    }
+};
+
+internals.checkEnv = function () {
+    if (!process.env.TP_PDF_TOOLS_NG_SERVER_JAR_PATH) {
+        throw new Error('TP_PDF_TOOLS_NG_SERVER_JAR_PATH must be defined');
+    }
+};
+
+internals.checkEnv();
+
+exports = module.exports = {
+    imageDiff: internals.getImageDiffOptions(),
+    ngServerJarPath: process.env.TP_PDF_TOOLS_NG_SERVER_JAR_PATH
+};
+
--- spec/runSpecs.js
+++ spec/runSpecs.js
@@ -1,9 +1,11 @@
 'use strict';
+const Path = require('path');
+require('dotenv').config({ path: Path.join(__dirname, '.env') });
 const Jasmine = require('jasmine');
 const JasmineSpecReporter = require('jasmine-spec-reporter');
 const NodeJasmineImageDiffTester = require('image-diff-tester/lib/nodeJasmine');
-const Yargs = require('yargs');
-
+const Config = require('./config');
+const NailgunReporter = require('./nailgunReporter');
 
 const jasmine = new Jasmine();
 jasmine.loadConfigFile('spec/support/jasmine.json');
@@ -11,39 +13,9 @@ jasmine.addReporter(new JasmineSpecReporter({
     displaySpecDuration: true
 }));
 
-/**
-    Either config image-diff-tester using command line arguments or using environment variables.
-
-    Rebase the default version
-
-        npm test -- --image-diff-rebase
-
-    Rebase another version
-
-        npm test -- --image-diff-rebase --image-diff-version ubuntu
-
- */
-const argv = Yargs.option('image-diff-rebase', {
-    type: 'boolean',
-    default: null
-}).option('image-diff-version', {
-    type: 'string',
-    nargs: 1,
-    demand: false,
-    default: null
-}).argv;
-
-const imageDiffBaseVersion = argv.imageDiffVersion || process.env.IMAGE_DIFF_BASE_VERSION || 'default';
-const imageDiffMode = (argv.imageDiffRebase ? 'rebase' : null) || process.env.IMAGE_DIFF_MODE || 'test';
-
-jasmine.addReporter(new NodeJasmineImageDiffTester({
-    baseDir: `images-diff/base/${imageDiffBaseVersion}`,
-    mode: imageDiffMode,
-    currentDir: 'images-diff/current',
-    diffDir: 'images-diff/diff',
-    mismatchThreshold: 0
-}));
-
-
+jasmine.addReporter(new NodeJasmineImageDiffTester(Config.imageDiff));
+jasmine.addReporter(new NailgunReporter());
 jasmine.execute();
+
+
 
